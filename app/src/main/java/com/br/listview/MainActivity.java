44dd42c2b68791720b79b48e072f371d3a083b6d
viniciusmaurente <viniciusvmaurente@gmail.com>
2020-02-10 10:14:48 -0300

    An alert dialog simple project implemented in Java with popup alert and Toast Short click button. This method can be used in your standard project to show an alert message at the click of a button.

---
+++ app/src/main/java/com/br/listview/MainActivity.java
@@ -0,0 +1,51 @@
+package com.br.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listLocais;
+    private String[] itens = {
+
+            "Amsterdam","Berlim","Dublin","Londres","Escócia",
+            "São Paulo","Curitiba","Veneza","Florianópolis","Pelotas","Porto Alegre",
+            "Paris","barcelona","Bruxelas","Galway"
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        listLocais = findViewById(R.id.listLocais);
+
+        //criar adaptador para a lista
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                itens
+        );
+
+        //adiciona adaptador para lista
+        listLocais.setAdapter(adaptador);
+
+        //adiciona clique na lista
+        listLocais.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String valorSelecionado = listLocais.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(),valorSelecionado,Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
